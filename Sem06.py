b92adf3a261a53da15616008f7b1dc2c593acdfd
Sergio Orizano Salvador <svorizano@gmail.com>
2024-10-14 21:29:33 -0500

    Realizando ejercicio 2

--- Sem06.py
+++ Sem06.py
@@ -8,3 +8,8 @@ st.subheader("Ejercicio 1: imprimir 'Hola Mundo' 10 veces")
 if st.button("Ejecutar Ejercicio 1"):
     for i in range(10):
         st.write("Hola Mundo")
+#Ejercicio 2: Imprimir los primeros 10 números
+st.subheader("Ejercicio 2: Imprimir los primeros 10 números")
+if st.button("Ejecuatar Ejercicio 2"):
+    for i in range(1, 11):
+        st.write(i)
