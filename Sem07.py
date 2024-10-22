f392d0ab05bbe5645c3820a1d75a9981b3e35c22
Sergio Orizano Salvador <svorizano@gmail.com>
2024-10-21 21:21:35 -0500

    activando semana 7

---
+++ Sem07.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+def mostrar_menu():
+    st.title("Ejemplo de Menú")
+    st.write("Selecciona una opción del menú")
+
+    menu = ["Archivo", "Editar", "Ver", "Salir"]
+    seleccion = ""
+
+    while seleccion != "Salir":
+        seleccion = st.radio("Menú", menu)
+
+        if seleccion == "Archivo":
+            st.write("Seleccionaste: Archivo")
+        elif seleccion == "Editar":
+            st.write("Seleccionaste: Editar")
+        elif seleccion == "Ver":
+            st.write("Seleccionaste: Ver")
+        elif seleccion == "Salir":
+            st.write("¡Saliendo del menú!")
+            break
+
+if __name__ == "__main__":
+    mostrar_menu()
