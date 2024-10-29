29dbdf18b453774f96d1f24f28b400f4aa335225
Sergio Orizano Salvador <svorizano@gmail.com>
2024-10-28 22:09:13 -0500

    ejecuntado 8.2

---
+++ Sem8_2.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+def calcular(operacion, num1, num2)
+    """Realiza la operación especificada entre num1 y num2."""
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+    except ValueError:
+        return "Por favor, ingrese números válidos"
+    
+    if operacion == "Suma":
+        return num1+num2
+    elif operacion == "Resta":
+        return num1-num2
+    elif operacion == "Multiplicación"
+        return num1*num2
+    elif operacion == "División"
+        if num2 == 0:
+            return "Error: no se puede dividir entre 0"
+        return num1/num2
+    else:
+        return "Operación no válida"
+
+def main():
+    st.title("Calculadora Básica")
+    st.write("Seleccione la operación e ingrese los números: ")
+
+    #Seleccione la operación
+    operacion = st.selectbox("Suma". "Resta", "Multiplicación", "División")
+
+    #Entradas para los números
+    num1 = st.text_input("Número 1")
+    num2 = st.text_input("Número 2")
+
+    #Botón para calcular
+    if st.button("Calcular")
+        resultado = calcular(operacion, nu1, num2)
+        st.write("**Resultado:**", resultado)
+
+if __name__ == "__main__"
+    main()
